4002f00cf070a79049b75c28058387848b1fc0ec
Kenward <umaterhemba@gmail.com>
2023-02-16 04:17:51 -0800

    5-printf.c update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - A program that print using printf function
  * Return: 0
  */
-init main(void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
